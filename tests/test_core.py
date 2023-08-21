38ff817e7ad00013454f448b0108b06d8240b963
Felippe F. Caso <felippe.felisola@gmail.com>
2023-08-20 22:26:18 -0300

    fix: add test_data_dir

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,12 +1,16 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
 
+from pathlib import Path
+
 from singer_sdk.testing import get_tap_test_class
 
 from tap_applehealth.tap import TapAppleHealth
 
+test_data_dir = Path(__file__).resolve().parent
+
 SAMPLE_CONFIG = {
-    "file_path": "/data/example_export.xml",
+    "file_path": f"{test_data_dir}/data/example_export.xml",
 }
 
 # Run standard built-in tap tests from the SDK:
